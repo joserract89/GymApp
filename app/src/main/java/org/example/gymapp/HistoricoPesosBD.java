6f6af2350ce283a705832d828398f778e4b2f2bc
joserract89 <joserract89@gmail.com>
2015-11-25 00:41:56 +0100

    Creo la clase para almacenar en BD

---
+++ app/src/main/java/org/example/gymapp/HistoricoPesosBD.java
@@ -0,0 +1,25 @@
+package org.example.gymapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hp1 on 25/11/2015.
+ */
+public class HistoricoPesosBD extends SQLiteOpenHelper {
+
+    public HistoricoPesosBD(Context context) {
+        super(context, "historico_pesos", bd, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
